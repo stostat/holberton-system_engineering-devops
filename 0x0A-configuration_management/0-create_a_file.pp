0ed596501904c5b69e48d4c7c4685a74970c6bd9
stostat <santmendieta@icloud.com>
2020-12-07 23:42:15 -0500

    added privuder to create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,12 @@
 #create a file with content
+
 file {'/tmp/holberton':
-      ensure => 'present',
-      path  => '/tmp/holberton',
-      mode  => '0744',
-      owner => 'www-data',
-      group => 'www-data',
-      content => 'I love puppet',
+
+      path    => '/tmp/holberton',
+      mode    => '0744',
+      owner   => 'www-data',
+      group   => 'www-data',
+      content  => 'I love puppet',
       provider => 'posix',
+
 }
