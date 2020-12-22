caba7f143a775b6dd94accf9cd429a70fac7260f
root <santmendieta@icloud.com>
2020-12-21 20:24:05 -0500

    puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,7 +5,13 @@ exec {'server-conf':
 }
 package { 'nginx':
   ensure => installed,
-  name   => 'nginx', # Double check
+  name   => 'nginx',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+  enable  => True,
 }
 
 file { '/var/www/html/index.html':
@@ -16,16 +22,12 @@ file_line { 'sites-default':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'root /var/www/html;',
-  # Could use a shell command but this is more clean
   line   => '	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
+
 file_line{ 'custom-http-header':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => '# as directory, then fall back to displaying a 404.',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'try_files $uri $uri/ =404;',
   line   => 'add_header X-Served-By "$HOSTNAME";'
-}
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
 }
